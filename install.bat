301bc496d4c037d1b56d9f4d3c023a1130e5e3e6
lukas1505 <lukas909009@gmail.com>
2021-05-25 21:40:15 +0200

    add install script for pip packages

---
+++ install.bat
@@ -0,0 +1,12 @@
+@ECHO OFF
+
+ECHO make sure the requirements in the github site is readed and all packages are successfully installed
+ECHO have fun and enjoy
+
+ECHO #####################################################################
+
+pip install SpeechRecognition
+pip install gtts
+pip install playsound
+
+ECHO please execute start.py
